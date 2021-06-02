1f56b091d395d649ea1ed55b3bf18aee8fb278d8
Rupesh Deshmukh <rupesh201098@gmail.com>
2021-06-01 22:50:57 -0400

    Comments and README.md

--- README.md
+++ README.md
@@ -1,8 +1,26 @@
-# Face Mask Detection using CNN
+# Face Mask Detection using Transfer Learning (VGG19)
 
-This project detects face masks in images and live video feeds. Currently, the code is set up to input computer's webcam feed. Dataset used for training CNN is available <a href='https://www.kaggle.com/ashishjangra27/face-mask-12k-images-dataset'>here</a>.
+This project detects face masks in images and live video feeds. Currently, the code is set up to input computer's webcam feed. Dataset used for training has been obtained from <a href='https://www.kaggle.com/ashishjangra27/face-mask-12k-images-dataset'>here</a>.
+
+## Training Model
+
+The model VGG19 has been trained to detect mask on a face. Learn more about Transfer Learning <a href='https://machinelearningmastery.com/how-to-use-transfer-learning-when-developing-convolutional-neural-network-models/'>here</a>.
 
 ## Detecting Faces
 
 To detect faces in an image or a frame of a video, we use <a href = 'https://github.com/opencv/opencv/tree/master/data/haarcascades'>haar cascade</a>'s frontal face default xml file.
+
+## Predicting on image/livefeed
+
+For an image, we simply extract all the faces from it and evaluate the faces using the model to predict whether it has a mask on it. For a video feed, we go through each frame to detect all the faces in it and evaluate each face detected with the model.
+
+## Results
+
+### With Mask
+
+<img src='images/WithMask.png'/>
+
+### Without Mask
+
+<img src='images/WithoutMask.png'/>
 
--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 
 import cv2
 import numpy as np
-# from keras.preprocessing.image import ImageDataGenerator
-# from keras.applications.vgg19 import VGG19
-# from keras import Sequential
+from keras.preprocessing.image import ImageDataGenerator
+from keras.applications.vgg19 import VGG19
+from keras import Sequential
 from keras.models import load_model
-# from keras.layers import Dense, Flatten
-# import warnings
-# warnings.filterwarnings("ignore")
+from keras.layers import Dense, Flatten
+import warnings
+warnings.filterwarnings("ignore")
 
+# Paths to data folders
 dataset_path = 'Face Mask Dataset/'
 train_path = dataset_path + 'Train'
 test_path = dataset_path + 'Test'
 validation_path = dataset_path + 'Validation'
 
+# Parameters to tweak
 img_size = (128, 128)
 batch_size = 32
 
@@ -34,15 +36,15 @@ model = Sequential([
 	Flatten(),
 	Dense(1, activation='sigmoid')
 ])
-model.summary()
+# model.summary()
 
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics='accuracy')
 history = model.fit_generator(generator=train_generator, steps_per_epoch=len(train_generator)//batch_size, epochs=20, validation_data=validation_generator, validation_steps=len(validation_generator)//batch_size)
-model.save('maskModel.h5')
+model.save('models/maskModel.h5')
 '''
 
-# Loading the trained model from memory
-model = load_model('maskModel.h5')
+# Loading the trained model
+model = load_model('models/maskModel.h5')
 
 # # Using haarcascade for face detection
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
@@ -51,41 +53,50 @@ face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
 while True:
+	# read input from camera
 	ret, img = cap.read()
 	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 	# Detecting faces in the frame
 	faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
+	# Dictionary for identifying predictions
 	mask_label = {0: 'MASK', 1: 'NO MASK'}
 
 	for x, y, w, h in faces:
 
-		# Drawing rectangle on frame around faces
-
-
-		#
+		# Getting face from original image
 		cropped_img = img[y:y+h, x:x+w]
+
+		# Transformations to match model input specifications
 		resized_img = cv2.resize(cropped_img, img_size)
 		normalized_img = resized_img/255.0
 		reshaped_img = np.reshape(normalized_img, (1, img_size[0], img_size[1], 3))
 		reshaped_img = np.vstack([reshaped_img])
+
+		# Predicting result
 		mask_result = round(model.predict(reshaped_img)[0][0])
 
+		# Set color to RED if NO MASK and GREEN if MASK is detected.
 		if mask_result:
 			display_color = (0, 0, 255)
 		else:
 			display_color = (0, 255, 0)
 
+		# Drawing rectangle on frame around the face
 		cv2.rectangle(img, (x, y), (x + w, y + h), display_color, 2)
+
+		# Displaying Text over the face
 		cv2.putText(img, mask_label[mask_result], (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 1, display_color, 2)
 
-	cv2.imshow('Image', img)
+	# Display image
+	cv2.imshow('Face Mask Detector', img)
 	k = cv2.waitKey(30)
 
-	# Break of Esc is pressed
+	# Break if Esc is pressed
 	if k == 27:
 		break
 
+# Release camera access and close all display windows
 cap.release()
 cv2.destroyAllWindows()
