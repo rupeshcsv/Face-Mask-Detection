26e2bf39e9b259673e7869eb40f08c8473811fed
Rupesh Deshmukh <rupesh201098@gmail.com>
2021-06-01 21:11:41 -0400

    trained model

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import cv2
 from keras.preprocessing.image import ImageDataGenerator
 from keras.applications.vgg19 import VGG19
 from keras import Sequential
+from keras.models import load_model
 from keras.layers import Dense, Flatten
 import warnings
 warnings.filterwarnings("ignore")
@@ -21,6 +22,8 @@ train_generator = datagen.flow_from_directory(train_path, target_size=img_size, 
 test_generator = datagen.flow_from_directory(test_path, target_size=img_size, batch_size=batch_size, class_mode='binary', color_mode='rgb')
 validation_generator = datagen.flow_from_directory(validation_path, target_size=img_size, batch_size=batch_size, class_mode='binary', color_mode='rgb')
 
+# Model can be trained using the lines below
+'''
 vgg19 = VGG19(weights='imagenet', include_top=False, input_shape=(img_size[0], img_size[1], 3))
 for layer in vgg19.layers:
 	layer.trainable = False
@@ -30,11 +33,15 @@ model = Sequential([
 	Flatten(),
 	Dense(1, activation='sigmoid')
 ])
-# model.summary()
+model.summary()
 
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics='accuracy')
-
 history = model.fit_generator(generator=train_generator, steps_per_epoch=len(train_generator)//batch_size, epochs=20, validation_data=validation_generator, validation_steps=len(validation_generator)//batch_size)
+model.save('maskModel.h5')
+'''
+
+# Loading the trained model from memory
+model = load_model('maskModel.h5')
 
 # # Using haarcascade for face detection
 # face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
